f132d880398d26fe097bab04f866f3bbac010587
puanazzahra <puan.az.2203226@students.um.ac.id>
2024-09-25 09:51:05 +0700

    Create suhu.py

---
+++ suhu.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+x = st.number_input ("Masukkan suhu")
+sx = st.text_input("Satuan", "C")
+st.write("Anda menginput suhu ", x, "dengan satuan ", sx)
+st.write("Kuadratnya ", x*x)
