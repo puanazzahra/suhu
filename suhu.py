2acce435508c9789a9714edec0393ead90706f69
puanazzahra <puan.az.2203226@students.um.ac.id>
2024-09-25 10:24:15 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -3,8 +3,8 @@ import streamlit as st
 x = st.number_input ("Masukkan suhu")
 sx = st.text_input("Satuan", "C")
 st.write("Anda menginput suhu ", x, "dengan satuan ", sx)
-sy = st.text_input("Konversi ke", "C")
-if (sx == "F"):
+sy = st.text_input("Konversi ke", "F")
+if (sx == "C"):
   pass
 else:
   pass
