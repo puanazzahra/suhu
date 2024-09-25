8ed647a8239a3e5d0767a2558aa40b6aaf1d63ea
puanazzahra <puan.az.2203226@students.um.ac.id>
2024-09-25 10:23:54 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -4,7 +4,7 @@ x = st.number_input ("Masukkan suhu")
 sx = st.text_input("Satuan", "C")
 st.write("Anda menginput suhu ", x, "dengan satuan ", sx)
 sy = st.text_input("Konversi ke", "C")
-if (sx == "C"):
+if (sx == "F"):
   pass
 else:
   pass
